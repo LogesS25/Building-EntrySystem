e220fdf5481760562d5e2a6eec19ba7bb34f3176
Loges <sivalog25@gmail.com>
2024-08-02 18:49:11 +0530

    changing port

--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://localhost:8000/api';
 
 export const registerEntry = (userId: string, entryGateId: string) => {
   return axios.post(`${API_URL}/entry`, { userId, entryGateId });
